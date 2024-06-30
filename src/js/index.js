d392e7c2fe0477d17b63db39abd0f5399136bc68
caamomilaa <c.velazquezdamico@gmail.com>
2024-07-01 00:05:17 +0200

    updte

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,8 @@
 // El styles lo importamos aquí para que se encargue Vite de compilar todo
-import '../scss/styles.scss';
+import "../scss/styles.scss";
+
+const nameElement = document.getElementById("name");
+const numberElement = document.getElementById("number");
+const monthElement = document.getElementById("month");
+const yearElement = document.getElementById("year");
+const cvcElement = document.getElementById("cvc");
