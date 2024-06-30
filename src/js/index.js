fbff47c43931efe71f41f7c9957052ea69c3a851
caamomilaa <c.velazquezdamico@gmail.com>
2024-07-01 00:23:37 +0200

    update

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,24 @@
 // El styles lo importamos aquí para que se encargue Vite de compilar todo
 import "../scss/styles.scss";
 
+const formElement = document.getElementById("form");
 const nameElement = document.getElementById("name");
 const numberElement = document.getElementById("number");
 const monthElement = document.getElementById("month");
 const yearElement = document.getElementById("year");
 const cvcElement = document.getElementById("cvc");
+const cardNameElement = document.getElementById("card-name");
+const cardNumberElement = document.getElementById("card-number");
+const cardMonthElement = document.getElementById("card-month");
+const cardYearElement = document.getElementById("card-year");
+
+const checkForm = (event) => {
+  event.preventDefault();
+};
+formElement.addEventListener("submit", checkForm);
+
+const introducingName = (event) => {
+  nameElement.value = cardNameElement.textContent;
+  console.log(introducingName);
+};
+nameElement.addEventListener("input", introducingName);
